a5bc54d27d290e5af8a2fb706a84d0216ffc439c
MaggGomes <magg.slb@gmail.cm>
2016-04-21 13:44:45 +0100

    no message

--- Project1/src/main.cpp
+++ Project1/src/main.cpp
@@ -4,6 +4,30 @@
 
 using namespace std;
 
+Graph<int> CreateTestGraph()
+		{
+	Graph<int> myGraph;
+
+	for(int i = 1; i < 8; i++)
+		myGraph.addVertex(i);
+
+	myGraph.addEdge(1, 2, 2);
+	myGraph.addEdge(1, 4, 7);
+	myGraph.addEdge(2, 4, 3);
+	myGraph.addEdge(2, 5, 5);
+	myGraph.addEdge(3, 1, 2);
+	myGraph.addEdge(3, 6, 5);
+	myGraph.addEdge(4, 3, 1);
+	myGraph.addEdge(4, 5, 1);
+	myGraph.addEdge(4, 6, 6);
+	myGraph.addEdge(4, 7, 4);
+	myGraph.addEdge(5, 7, 2);
+	myGraph.addEdge(6, 4, 3);
+	myGraph.addEdge(7, 6, 4);
+
+	return myGraph;
+		}
+
 int main(){
 	/*GraphViewer *gv = new GraphViewer(600, 600, false);
 	gv->createWindow(600, 600);
@@ -12,16 +36,14 @@ int main(){
 	gv->rearrange();*/
 
 
-	int tempID = 1;
-	//txtCReader(tempID);
-	//cout << deg_to_cartLat(41.1800) << " - " << deg_to_cartLong(-8.600) << endl ;
+	/*int tempID = 1;
 
 	GraphViewer *gv = graphCreator(txtAReader(),txtBReader() ,txtCReader(tempID));
 	gv->rearrange();
 	getchar();
 
 	SchoolBus schoolBus;
-	schoolBus.menuStarting();
+	schoolBus.menuStarting();*/
 
 
 
