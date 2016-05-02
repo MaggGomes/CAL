b9f85eb190346d8a1aa778815fda6425a9447335
joaocastro <up201404896@fe.up.pt>
2016-05-02 18:35:02 +0100

    Menu fixed

--- Project1/src/SchoolBus.cpp
+++ Project1/src/SchoolBus.cpp
@@ -511,7 +511,7 @@ void SchoolBus::menuStarting(){
 		setColor(11, 0);
 		cout << setw(51) << "<<<<<   WELCOME    >>>>>" << endl << endl;
 
-		for (int i = 0; i < 6; ++i)
+		for (int i = 0; i < 7; ++i)
 		{
 			if (i == pointer)
 			{
